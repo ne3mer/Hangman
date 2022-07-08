f393f690aa0fa517cf373bda358b90e4e22aa379
nima afsharfar <ne3mer@gmail.com>
2022-07-09 04:03:24 +0430

    publish hangman

---
+++ Hangman.py
@@ -0,0 +1,55 @@
+import random
+from kalamat import kalamat
+from scaffold import print_scaffold
+# scafold hangman art
+
+
+
+
+
+# Create a list for choose from it randomely
+list = kalamat
+
+
+# choose a random var from list
+randomChoose = random.choice(list)
+
+# lives
+lives = 0
+
+# print a list of "_" for the len of our random choice
+emptyList = []
+for i in range(len(randomChoose)):
+    emptyList.append("_")
+print(emptyList)
+
+endOfTheGame = False
+
+while not endOfTheGame:
+    # get a char from user
+
+    guess = input("type your char:").lower()
+
+    if guess in emptyList:
+        print(f"{guess} ro  ghablan gofte boodi!!!!!!!!")
+
+    # check if input char is excist in randomchoice
+    for i in range(len(randomChoose)):
+        letter = randomChoose[i]
+        if letter == guess:
+            emptyList[i] = letter
+
+    if guess not in randomChoose:
+        lives += 1
+        print("not match")
+
+    print(emptyList)
+    print_scaffold(lives)
+
+    if lives == 6:
+        print(f"you loose!!!!!!/n the right word was {randomChoose.upper()}")
+        break
+
+    if "_" not in emptyList:
+        endOfTheGame = True
+        print(f"you won")
